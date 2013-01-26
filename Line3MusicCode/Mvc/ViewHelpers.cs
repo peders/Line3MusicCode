ff9c867cf204db7420ea00d783258c9d9124ff5d
Peder Skou <peder.skou@gyldendal.no>
2013-01-26 13:46:35 +0100

    Now with code

---
+++ Line3MusicCode/Mvc/ViewHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Line3MusicCode.Mvc
+{
+    public class ViewHelpers
+    {
+        public static string ControllerName(this ViewContext source)
+        {
+            var name = source.RouteData.Values["controller"] as string;
+            if (String.IsNullOrWhiteSpace(name)) return null;
+            return name;
+        }
+
+        public static string ActionName(this ViewContext source)
+        {
+            var name = source.RouteData.Values["action"] as string;
+            if (String.IsNullOrWhiteSpace(name)) return null;
+            return name;
+        }
+    }
+}
